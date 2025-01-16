69df0216d33ce3e00ae6f3641d761e1408f316f6
namahoroleochristian <leonamahoro1@gmail.com>
2025-01-15 18:58:12 -0800

    "contract to analyse largest integer between 3"

---
+++ contracts/Largest.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8;
+// SPDX-Licence-Identifier :MIT;
+contract Largest{
+    function CheckLargest(int Number1,int Number2,int Number3) public pure returns(string memory result){
+        if (Number1 > Number2 && Number1 >Number3){
+            result="Number 1 is the biggest of all";
+            return result;
+        }
+        else if (Number2 > Number1 && Number2 >Number3){
+            result="Number 2 is the biggest of all";
+            return result;
+        }
+        else {
+            result="Number 3 is the biggest of all";
+            return result;
+
+        }
+    }
+}

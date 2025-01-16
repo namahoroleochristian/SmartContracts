4548646232f0cfb5653af260645ca47877980193
namahoroleochristian <leonamahoro1@gmail.com>
2025-01-15 19:49:23 -0800

    "Using Uint Array to set and retrieve numbers"

--- contracts/Table.sol
+++ contracts/Table.sol
@@ -1,11 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^ 0.8;
-contract Table {
-    function getTable(uint a) public  pure returns(uint[10] memory) {
-        uint[10] memory Table;
-        for(uint i = 0 ; i < 10 ; i++){
-            Table[i] = a * (i+1);
+// contract Table {
+//     function getTable(uint a) public  pure returns(uint[10] memory) {
+//         uint[10] memory Table;
+//         for(uint i = 0 ; i < 10 ; i++){
+//             Table[i] = a * (i+1);
+//         }
+//         return Table;
+//     }
+// }
+
+
+contract DisplayNumbers {
+    function seeNumbers() public pure returns (uint[] memory) {
+        uint[] memory numbers; // declare an array of size 101, from index 0-100.
+        for (uint i = 0; i <= 100; i++) { 
+            numbers[i] = i;
         }
-        return Table;
-    }
+        return numbers; 
+    }   
 }
